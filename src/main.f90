5c43c077de88cfc28aa4087ddbdeb76db0d7e669
Mauri Favaron <mafavaron@mac.com>
2019-09-10 21:11:52 +0200

    Update main.f90
    Command line processing added

--- src/main.f90
+++ src/main.f90
@@ -3,4 +3,27 @@
 !
 program SonicProcess
 
+  implicit none
+
+  ! Locals
+  character(len=256)  :: sInputPath
+  character(len=256)  :: sOutputPath
+  integer             :: iRetCode
+
+  ! Get command arguments
+  if(command_argument_count() /= 2) then
+    print *, "error:: Invalid command line"
+    print *
+    print *, "Usage:"
+    print *
+    print *, "  ./fp <Input_Path> <Output_Path>"
+    print *
+    print *, "Copyright 2019 by Servizi Territorio srl"
+    print *, "                  This software is open source, covered by the MIT license"
+    print *
+    stop
+  end if
+  call get_command_argument(1, sInputPath)
+  call get_command_argument(2, sOutputPath)
+
 end program SonicProcess
