7e5fc9a7b24b6fc47a51a209e4328194328388c0
Mauri Favaron <mafavaron@mac.com>
2019-09-11 22:42:58 +0200

    Update main.f90
    Execution flow adapted to the flat directory SonicLib data is usually in

--- src/main.f90
+++ src/main.f90
@@ -13,12 +13,11 @@ program SonicEncode
   integer             :: iRetCode
   character(len=256)  :: sSubdir
   type(file$info)     :: tFileInfo
-  type(file$info)     :: tFileInfo2
   integer(4)          :: iHandle
   integer(4)          :: iFileHandle
   integer(4)          :: iLength
   character(len=256)  :: sInputFileName
-  character(len=256)  :: sFileName
+  character(len=256)  :: sOutputFileName
   character(len=256)  :: sBuffer
   integer             :: iPos
   integer             :: iNumData
@@ -51,73 +50,59 @@ program SonicEncode
   do
     iLength = getfileinfoqq(trim(sInputPath) // "\\*", tFileInfo, iHandle)
     if(iHandle == file$last .or. iHandle == file$error) exit
-    if((tFileInfo % permit .and. file$dir) /= 0) then
-      if(len_trim(tFileInfo % name) == 6) then
-        sSubDir = trim(sInputPath) // '\\' // trim(tFileInfo % name)
+    if((tFileInfo % permit .and. file$dir) == 0) then
+      sInputFileName = trim(sInputPath) // '\\' // trim(tFileInfo % name)
+      sOutputFileName = sInputFileName(:len_trim(sInputFileName)-3) // "fsn"
 
-        ! Identify files in sub-directories
-        iFileHandle = file$first
-        do
-
-          ! Get input and output file names
-          iLength = getfileinfoqq(trim(sSubDir) // "\\*", tFileInfo2, iFileHandle)
-          if(iFileHandle == file$last .or. iFileHandle == file$error) exit
-          if(len_trim(tFileInfo2 % name) /= 11) cycle
-          sInputFileName = trim(sSubDir) // '\\' // trim(tFileInfo2 % name)
-          sFileName = trim(sOutputPath) // '\\' // trim(tFileInfo2 % name) // '.slb'
-
-          ! Process file
-          ! -1- Count lines
-          print *, "Encoding ", trim(sInputFileName)
-          open(10, file=sInputFileName, status='old', action='read', iostat=iRetCode)
-          if(iRetCode /= 0) then
-            print *,trim(sInputFileName)
-            print *, 'error:: Input file not opened - ', iRetCode
-            stop
-          end if
-          ! -1- Count data in file
-          read(10, "(a)", iostat=iRetCode) sBuffer
-          if(iRetCode /= 0) then
-            print *, 'error:: Empty input file'
-            stop
-          end if
-          iNumData = 0
-          do
-            read(10, "(a)", iostat=iRetCode) sBuffer
-            if(iRetCode /= 0) exit
-            iNumData = iNumData + 1
-          end do
-          if(iNumData <= 0) then
-            print *, 'error:: No data in input file'
-            stop
-          end if
-          ! -1- Reserve workspace
-          if(allocated(ivTimeStamp)) deallocate(ivTimeStamp)
-          allocate(ivTimeStamp(iNumData))
-          if(allocated(ivU)) deallocate(ivU)
-          allocate(ivU(iNumData))
-          if(allocated(ivV)) deallocate(ivV)
-          allocate(ivV(iNumData))
-          if(allocated(ivW)) deallocate(ivW)
-          allocate(ivW(iNumData))
-          if(allocated(ivT)) deallocate(ivT)
-          allocate(ivT(iNumData))
-          ! -1- Really read data
-          rewind(10)
-          read(10, "(a)") sBuffer
-          do iData = 1, iNumData
-            read(10, *) iTimeStamp, rU, rV, rW, rT
-            ivTimeStamp(iData) = iTimeStamp
-            ivU(iData) = nint(rU * 100.0)
-            ivV(iData) = nint(rV * 100.0)
-            ivW(iData) = nint(rW * 100.0)
-            ivT(iData) = nint(rT * 100.0)
-          end do
-          close(10)
-
-        end do
-
+      ! Process file
+      ! -1- Count lines
+      print *, "Encoding ", trim(sInputFileName)
+      open(10, file=sInputFileName, status='old', action='read', iostat=iRetCode)
+      if(iRetCode /= 0) then
+        print *,trim(sInputFileName)
+        print *, 'error:: Input file not opened - ', iRetCode
+        stop
       end if
+      ! -1- Count data in file
+      read(10, "(a)", iostat=iRetCode) sBuffer
+      if(iRetCode /= 0) then
+        print *, 'error:: Empty input file'
+        stop
+      end if
+      iNumData = 0
+      do
+        read(10, "(a)", iostat=iRetCode) sBuffer
+        if(iRetCode /= 0) exit
+        iNumData = iNumData + 1
+      end do
+      if(iNumData <= 0) then
+        print *, 'error:: No data in input file'
+        stop
+      end if
+      ! -1- Reserve workspace
+      if(allocated(ivTimeStamp)) deallocate(ivTimeStamp)
+      allocate(ivTimeStamp(iNumData))
+      if(allocated(ivU)) deallocate(ivU)
+      allocate(ivU(iNumData))
+      if(allocated(ivV)) deallocate(ivV)
+      allocate(ivV(iNumData))
+      if(allocated(ivW)) deallocate(ivW)
+      allocate(ivW(iNumData))
+      if(allocated(ivT)) deallocate(ivT)
+      allocate(ivT(iNumData))
+      ! -1- Really read data
+      rewind(10)
+      read(10, "(a)") sBuffer
+      do iData = 1, iNumData
+        read(10, *) iTimeStamp, rU, rV, rW, rT
+        ivTimeStamp(iData) = iTimeStamp
+        ivU(iData) = nint(rU * 100.0)
+        ivV(iData) = nint(rV * 100.0)
+        ivW(iData) = nint(rW * 100.0)
+        ivT(iData) = nint(rT * 100.0)
+      end do
+      close(10)
+
     end if
   end do
 
