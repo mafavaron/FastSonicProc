f2ea0c2a31d6104b1c742efc41047dce1ae87b82
Mauri Favaron <mafavaron@mac.com>
2019-09-13 08:35:19 +0200

    Create files.f90
    File support (in progress)

---
+++ src/files.f90
@@ -0,0 +1,70 @@
+! files.f90 - Module, incorporating directory scans and access to FastSonic files
+!
+! Copyright 2019 by Servizi Territorio srl
+!                   This is open-source code, covered by the MIT license
+!
+! Written by: PM-62
+!
+module files
+
+  implicit none
+
+  private
+
+  ! Public constants
+  integer, parameter  :: PATH$FLAT  = 1
+  integer, parameter  :: PATH$METEK = 2
+
+  ! Public interface
+  public  :: FindDataFiles
+  Public  :: PATH$FLAT
+  Public  :: PATH$METEK
+  public  :: ReadFastSonicFile
+
+contains
+
+  ! Map FastSonic files in a user-given directory
+  function FindDataFiles(sInputPath, svFiles, iPathType, lUnixDelimiter) result(iRetCode)
+
+    ! Routine arguments
+    character(len=*), intent(in)                                :: sInputPath
+    character(len=256), dimension(:), allocatable, intent(out)  :: svFiles
+    integer, optional, intent(in)                               :: iPathType      ! Default: PATH$METEK
+    character, optional, intent(in)                             :: lUnixDelimiter ! Default: .TRUE.
+    integer                                                     :: iRetCode
+
+    ! Locals
+    integer   :: iPathStructure
+    character :: cDelim
+
+    ! Assume success (will falsify on failure)
+    iRetCode = 0
+
+    ! Set path structure based on 'iPathType'
+    if(present(iPathType)) then
+      iPathStructure = iPathType
+    else
+      iPathStructure = PATH$METEK
+    end if
+
+    ! Set path delimiter based on 'lUnixDelimiter'
+    if(present(lUnixDelimiter)) then
+      if(lUnixDelimiter) then
+        cDelim = '/'
+      else
+        cDelim = '\\' ! Single-char \\, assuming "non-UNIX" actually means "Microsoft Windows"
+      end if
+    else
+      cDelim = '/'
+    end if
+
+    ! Dispatch execution based on 'iPathStructure'
+    select case(iPathStructure)
+    case(PATH$FLAT)
+    case(PATH$METEK)
+    case default
+    end select
+
+  end function FindDataFiles
+
+end module files
