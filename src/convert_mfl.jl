9e0d4fa6879e70773d274ade3c88916232b69054
Mauri Favaron <mafavaron@mac.com>
2019-09-20 12:04:05 +0200

    Update convert_mfl.jl
    Placeholder for WindRecorder path added, and very first exe node added. Answers issue #10 .

--- src/convert_mfl.jl
+++ src/convert_mfl.jl
@@ -461,6 +461,231 @@ elseif sRawDataForm == "MFC2"   # MeteoFlux Core V2
 
 elseif sRawDataForm == "WR"     # WindRecorder
 
+    # Main loop: Iterate over files
+    for f in svFiles
+
+        # Get data
+        lines = readlines(f)
+        print(length(lines))
+        exit(8)
+        lines = split(data[1], '\r')
+
+        # Secondary loop: Iterate over lines in file
+        U = Float32[]
+        V = Float32[]
+        W = Float32[]
+        T = Float32[]
+        analogData = Float32[]
+        firstLine = true
+        iU = -9999
+        iV = -9999
+        iW = -9999
+        iT = -9999
+        analog = [-9999, -9999, -9999, -9999, -9999, -9999, -9999, -9999, -9999, -9999]
+        numLines = length(lines)
+        if lines[numLines] == ""
+            numLines -= 1
+        end
+        lineType = -1
+        numInvalid = 0
+        numValid   = 0
+        if numLines > 0
+            for lineIdx in 1:numLines
+
+                # Divide line in parts, along ',' separator
+                line = lines[lineIdx]
+                fields = split(line, ',')
+                numFields = size(fields)[1]
+
+                # Check the string is potentially valid, that is, it contains two comma-separated blocks...
+                iValue1 = -99999
+                iValue2 = -99999
+                iValue3 = -99999
+                iValue4 = -99999
+                valid   = false
+                numCommas = numFields - 1
+                if numFields == 2
+                    dataString = fields[2,1]
+
+                    # ... and the length of second block is 42 characters
+                    if length(dataString) != 42
+                        println(dia, @sprintf(" -W- Data line len. not 42 - Line: %6d -> %s", lineIdx, line))
+                        lineType = guessLineType(dataString)
+                        if lineType == 1
+                            dataString = " M:x = -9999 y = -9999 z = -9999 t = -9999"
+                        elseif lineType == 2
+                            dataString = " M:e1= -9999 e2= -9999 e3= -9999 e4= -9999"
+                        elseif lineType == 3
+                            dataString = " M:e5= -9999 e6= -9999 e7= -9999 e8= -9999"
+                        elseif lineType == 4
+                            dataString = " M:c1= -9999 c2= -9999"
+                        end
+                        numInvalid += 1
+                    else
+
+                        # At this point, line is two comma-separated blocks and second block is 42 characters.
+                        # This does not still mean the string contents makes sense: a direct pareseability check
+                        # is due.
+                        try
+                            iValue1 = parse(Int, dataString[ 7:12])
+                            iValue2 = parse(Int, dataString[17:22])
+                            iValue3 = parse(Int, dataString[27:32])
+                            iValue4 = parse(Int, dataString[37:42])
+                            numValid += 1
+                            valid    = true
+                            lineType = getLineType(dataString)
+                        catch e
+                            println(dia, @sprintf(" -W- Not parsing to numbers - Line: %6d ->%s", lineIdx, line))
+                            iValue1 = -9999
+                            iValue2 = -9999
+                            iValue3 = -9999
+                            iValue4 = -9999
+                            lineType = guessLineType(dataString)
+                            if guessedLineType == 1
+                                dataString = " M:x = -9999 y = -9999 z = -9999 t = -9999"
+                            elseif guessedLineType == 2
+                                dataString = " M:e1= -9999 e2= -9999 e3= -9999 e4= -9999"
+                            elseif guessedLineType == 3
+                                dataString = " M:e5= -9999 e6= -9999 e7= -9999 e8= -9999"
+                            elseif guessedLineType == 4
+                                dataString = " M:c1= -9999 c2= -9999"
+                            end
+                            numInvalid += 1
+                        end
+
+                    end
+
+                else
+
+                    # Too few, or too many, commas
+                    println(dia, @sprintf(" -W- Num.commas not 1 - Line: %6d -> %s", lineIdx, line))
+                    lineType = guessLineType(line)
+                    if lineType == 1
+                        dataString = " M:x = -9999 y = -9999 z = -9999 t = -9999"
+                    elseif lineType == 2
+                        dataString = " M:e1= -9999 e2= -9999 e3= -9999 e4= -9999"
+                    elseif lineType == 3
+                        dataString = " M:e5= -9999 e6= -9999 e7= -9999 e8= -9999"
+                    elseif lineType == 4
+                        dataString = " M:c1= -9999 c2= -9999"
+                    end
+                    numInvalid += 1
+
+                end
+                lastLineQuadruple = false
+                if lineType == 1
+                    if !firstLine
+                        rU, rV, rW, rT, analogConverted = encode(iU, iV, iW, iT, analog, rvMultiplier, rvOffset, rvMinPlausible, rvMaxPlausible)
+                        if sInvalidDataFate == "ExcludeSonicQuadruples"
+                            if rU >= -9990.0f0 && rV >= -9990.0f0 && rW >= -9990.0f0 && rT >= -9990.0f0
+                                append!(U, rU)
+                                append!(V, rV)
+                                append!(W, rW)
+                                append!(T, rT)
+                                append!(analogData, analogConverted)
+                            end
+                        elseif sInvalidDataFate == "ExcludeAll"
+                            if rU >= -9990.0f0 && rV >= -9990.0f0 && rW >= -9990.0f0 && rT >= -9990.0f0 && all(x -> x >= -9990.0f0, analogConverted)
+                                append!(U, rU)
+                                append!(V, rV)
+                                append!(W, rW)
+                                append!(T, rT)
+                                append!(analogData, analogConverted)
+                            end
+                        elseif sInvalidDataRate == "Keep"
+                            append!(U, rU)
+                            append!(V, rV)
+                            append!(W, rW)
+                            append!(T, rT)
+                            append!(analogData, analogConverted)
+                        else
+                            append!(U, rU)
+                            append!(V, rV)
+                            append!(W, rW)
+                            append!(T, rT)
+                            append!(analogData, analogConverted)
+                        end
+                    end
+                    # Start a new line
+                    firstLine = false
+                    if flipReference
+                        # USA-1 and old uSonic-3
+                        iU = iValue2
+                        iV = iValue1
+                    else
+                        # uSonic-3
+                        iU = iValue1
+                        iV = iValue2
+                    end
+                    iW = iValue3
+                    iT = iValue4
+                    analog = [-9999, -9999, -9999, -9999, -9999, -9999, -9999, -9999, -9999, -9999]
+                    lastLineQuadruple = true
+                elseif lineType == 2
+                    analog[ 1] = iValue1
+                    analog[ 2] = iValue2
+                    analog[ 3] = iValue3
+                    analog[ 4] = iValue4
+                elseif lineType == 3
+                    analog[ 5] = iValue1
+                    analog[ 6] = iValue2
+                    analog[ 7] = iValue3
+                    analog[ 8] = iValue4
+                    lastLineQuadruple = false
+                elseif lineType == 4
+                    analog[ 9] = iValue1
+                    analog[10] = iValue2
+                    lastLineQuadruple = false
+                end
+            end
+            if lineType != 1
+                rU, rV, rW, rT, analogConverted = encode(iU, iV, iW, iT, analog, rvMultiplier, rvOffset, rvMinPlausible, rvMaxPlausible)
+                append!(U, rU)
+                append!(V, rV)
+                append!(W, rW)
+                append!(T, rT)
+                append!(analogData, analogConverted)
+            end
+            println(dia, f, @sprintf(" -I- %6d data lines parsed", length(U)))
+        else
+            println(dia, f, " -E- No data in file")
+        end
+
+        # Generate floating point time stamps
+        deltaTime = 3600.0f0 / numLines
+        if length(U) > 1
+            timeStamp = collect(range(0.0f0, stop=3600.0f0 - deltaTime, length=length(U)))
+        elseif length(U) == 1
+            timeStamp = [1800.0f0]
+        else
+            timeStamp = []
+        end
+
+        # Write to file
+        sOutputFileName = generateOutFileName(f, sTypeOfPath, sOutputPath, separator)
+        nQuantities = length(analogConverted)
+        n = length(timeStamp)
+        g = open(sOutputFileName, "w")
+        write(g, Int32(n))
+        write(g, Int16(nQuantities))
+        for i in 1:nQuantities
+            write(g, ascii((svName[i] * "        ")[1:8]))
+        end
+        write(g, timeStamp)
+        write(g, U)
+        write(g, V)
+        write(g, W)
+        write(g, T)
+        for i in 1:nQuantities
+            write(g, analogData[i:nQuantities:n*nQuantities])
+        end
+        close(g)
+
+        println(f, ", Lines=", numLines, ", Records=", n, ", Valid.Lines=", numValid, ", Invalid.Lines=", numInvalid)
+        println(rep, f, ", ", numLines, ", ", n, ", ", numValid, ", ", numInvalid)
+
+    end
+
 else
 
     println("error: Parameter 'RawDataForm' in configuration file is not MFCL, MFC2, or WR")
