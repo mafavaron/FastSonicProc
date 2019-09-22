fe77d9dae657f4586baa5bddc9a239eea39f7bd3
Mauri Favaron <mafavaron@mac.com>
2019-09-22 11:21:43 +0200

    Update stats.f90
    In progress - Interface reviewed, and time index now computed

--- src/stats.f90
+++ src/stats.f90
@@ -17,9 +17,10 @@ module Stats
 
 continue
 
-  function mean(rvValues, iAveraging, rvMean, lRemoveInvalid, rLower, rUpper) result(iRetCode)
+  function mean(rvTimeStamp, rvValues, iAveraging, rvMean, lRemoveInvalid, rLower, rUpper) result(iRetCode)
 
     ! Routine arguments
+    real, dimension(:), intent(in)                :: rvTimeStamp    ! "Small" vector (i.e. not more than about 100000 elements) of time stamps (secs within hour)
     real, dimension(:), intent(in)                :: rvValues       ! "Small" vector (i.e. not more than about 100000 elements) of values to average
     integer, intent(in)                           :: iAveraging     ! In range 1..3600; better, but not strictly necessary, if a divisor of 3600
     real, dimension(:), allocatable, intent(out)  :: rvMean         ! Average values
@@ -30,22 +31,39 @@ continue
 
     ! Locals
     real, dimension(size(rvValues)), device     :: rv_d_Values
-    integer, dimension(size(rvValues)), device  :: ivNumValues
+    integer, dimension(size(rvValues)), device  :: ivTimeIndex
     integer                                     :: iNumValues
+    integer                                     :: i
 
     ! CUDA-related constants
     integer, parameter  :: tPB = 256
 
+    ! Assume success (will falsify on failure)
+    iRetCode = 0
+
     ! check parameters
     if(.not.allocated(rvValues)) then
       if(allocated(rvMean)) deallocate(rvMean)
+      iRetCode = 1
+      return
     end if
     if(size(rvValues) <= 0) then
       if(allocated(rvMean)) deallocate(rvMean)
+      iRetCode = 2
+      return
     end if
     if(iAveraging <= 0 .or. iAveraging > 3600) then
       if(allocated(rvMean)) deallocate(rvMean)
+      iRetCode = 3
+      return
     end if
+
+    ! First step: generate the time index of all values
+    iNumValues = size(rvTimeStamp)
+    !$cuf kernel do <<<*,tPB>>>
+    do i = 1, iNumValues
+      ivTimeIndex(i) = floor(rvTimeStamp(i) / iAveraging)
+    end do
 
   end function mean
 
