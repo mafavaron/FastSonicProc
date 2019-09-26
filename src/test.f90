6e5d1c5bcdc60f44a983354d45109ecf9726d0f8
Mauri Favaron <mafavaron@mac.com>
2019-09-26 07:41:44 +0200

    Update test.f90
    Example of reduction code added

--- src/test.f90
+++ src/test.f90
@@ -1,18 +1,24 @@
 program test
 
-  use cudafor
+  use cudafor, gpusum => sum
 
   implicit none
 
   ! Locals
-  real, dimension(:), allocatable         :: a
-  real, dimension(:), allocatable, device :: d_a
-  integer                                 :: numElements
+  real, dimension(:), allocatable          :: a
+  real, dimension(:), allocatable, managed :: d_a
+  integer                                  :: numElements
+  integer                                  :: iState
 
   ! Initialize
   numElements = 128
   allocate(a(numElements))
   allocate(d_a(numElements))
+
+  d_a = 1.0
+  iState = cudaDeviceSynchronize()
+  print *, sum(d_a)
+  print *, gpusum(d_a)
 
   ! Leave
   deallocate(d_a)
